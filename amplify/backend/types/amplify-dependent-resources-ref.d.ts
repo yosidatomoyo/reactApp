5ed9b3f58f350fe07e91ec8dd325cbb6f94ea5ae
吉田知代 <yoshidatomoyo@yoshidatomoyonoiMac.local>
2023-01-28 14:48:02 +0900

    “認証機能追加”

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "reactapp8e141873": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
